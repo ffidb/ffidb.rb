94cbf1d33eaa62484ebefdeffef78e8f78f72b96
Arto Bendiken <arto@bendiken.net>
2020-05-21 07:05:57 +0300

    Implemented template-based C++ code generation.

--- lib/ffidb/exporters/cpp.rb
+++ lib/ffidb/exporters/cpp.rb
@@ -6,28 +6,8 @@ module FFIDB::Exporters
   ##
   # Code generator for the C++ programming language.
   class Cpp < C
-    SYMBOL_INDENT    = 2
-    EXTERN_QUALIFIER = 'extern "C"'
-
-    def begin
-      puts "// #{FFIDB.header}" if self.header?
-      puts if self.header?
-      puts "#include <cstdarg>     // for va_list"
-      puts "#include <cstdbool>    // for bool" # not needed in C++
-      puts "#include <cstddef>     // for size_t"
-      puts "#include <cstdint>     // for {,u}int*_t"
-      puts "#include <sys/types.h> // for off_t"
-    end
-
-    def begin_library(library)
-      @library = library
-      @namespace = self.options[:module] || library.name
-      puts
-      puts "namespace #{@namespace} {"
-    end
-
-    def finish_library
-      puts "} // #{@namespace}"
+    def finish
+      puts self.render_template('cpp.erb')
     end
   end # Cpp
 end # FFIDB::Exporters
